3fd16fea91ba5d2e411222ab783975ef2f6faff9
Dino Babić <dinobabi3@gmail.com>
2022-11-13 20:09:27 +0100

    Adding the Vote and Comment join tables

---
+++ freshVotes/src/main/java/com/freshVotes/domain/Vote.java
@@ -0,0 +1,33 @@
+package com.freshVotes.domain;
+
+import javax.persistence.Embeddable;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "vote")
+public class Vote {
+
+	private VoteId pk; // primary key
+	private Boolean upvote;
+
+	public Boolean getUpvote() {
+		return upvote;
+	}
+
+	public void setUpvote(Boolean upvote) {
+		this.upvote = upvote;
+	}
+	
+	@EmbeddedId
+	public VoteId getPk() {
+		return pk;
+	}
+
+	public void setPk(VoteId pk) {
+		this.pk = pk;
+	}
+	
+	 
+}
---
+++ freshVotes/src/main/java/com/freshVotes/domain/VoteId.java
@@ -0,0 +1,32 @@
+package com.freshVotes.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.Embeddable;
+import javax.persistence.ManyToOne;
+
+@Embeddable
+public class VoteId implements Serializable {
+	
+	private static final long serialVersionUID = -955091484361158055L;
+	private User user;
+	private Feature feature;
+	
+	@ManyToOne
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+	
+	@ManyToOne
+	public Feature getFeature() {
+		return feature;
+	}
+	public void setFeature(Feature feature) {
+		this.feature = feature;
+	}
+	
+	
+}
