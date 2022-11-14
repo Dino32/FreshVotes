7c7cb51b80fba30de28ac472f0152611d85d2b19
Dino Babić <dinobabi3@gmail.com>
2022-11-14 23:02:36 +0100

    adding password encoder

--- freshVotes/src/main/java/com/freshVotes/security/WebSecurityConfiguration.java
+++ freshVotes/src/main/java/com/freshVotes/security/WebSecurityConfiguration.java
@@ -1,18 +1,27 @@
 package com.freshVotes.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 	
+	@Bean
+	public PasswordEncoder getPasswordEncoder()  {
+		return new BCryptPasswordEncoder();
+	}
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.inMemoryAuthentication()
+			.passwordEncoder(getPasswordEncoder())
 			.withUser("db53796@fer.hr")
-			.password("ujLG5xV4")
+			.password(getPasswordEncoder().encode("ujLG5xV4"))
 			.roles("USER");
 	}
 	
@@ -20,8 +29,8 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable()
 			.authorizeRequests()
-				.antMatchers("/").permitAll()
-				.anyRequest().hasRole("USER")
+				.antMatchers("/").permitAll() // allows every user to see / root
+				.anyRequest().hasRole("USER") // fore any other request user needs to have USER role
 			.and()
 			.formLogin()
 				.loginPage("/login")
