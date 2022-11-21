5aa8adabcce68b339768f2d352b8cd7d694edb29
Dino Babić <dinobabi3@gmail.com>
2022-11-21 23:42:59 +0100

    Saving and updating Product

--- freshVotes/src/main/java/com/freshVotes/web/ProductController.java
+++ freshVotes/src/main/java/com/freshVotes/web/ProductController.java
@@ -1,10 +1,17 @@
 package com.freshVotes.web;
 
+import java.io.IOException;
+import java.util.Optional;
+
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -24,7 +31,18 @@ public class ProductController {
 	}
 	
 	@GetMapping("/products/{productId}")
-	public String getProduct(@PathVariable Long productId) {
+	public String getProduct(@PathVariable Long productId, ModelMap model, HttpServletResponse response) throws IOException{
+		Optional<Product> productOpt = productRepository.findById(productId);
+		
+		if (productOpt.isPresent()) {
+			Product product = productOpt.get();
+			model.put("product", product);
+		}
+		else {
+			response.sendError(HttpStatus.NOT_FOUND.value(), "Product with id " + productId + " was not found.");
+			return "product";
+		}
+		
 		return "product";
 	}
 	
@@ -39,4 +57,15 @@ public class ProductController {
 		
 		return "redirect:/products/" + product.getId();
 	}
+	
+	@PostMapping("products/{productId}")
+	public String saveProduct(@PathVariable Long productId, Product product) {
+		productRepository.save(product);
+		return "redirect:/products/" + product.getId();
+	}
 }
+
+
+
+
+
